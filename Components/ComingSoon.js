8d2c23d9d5a19aad0d14b6fb0be3fe03f5a17253
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2024-02-04 00:15:10 +0530

    added coming soon page (initial commit)

---
+++ Components/ComingSoon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ComingSoon = () => {
+    return(
+        <div className='main'>
+            <h1 id='name'>Aman Sharma</h1>
+            <hr />
+            <h1 id='portfolio'>Portfolio</h1>
+            <h1 id='soon'>Co<span id='two'>m</span><span id='one'>ing Soon...</span></h1>
+        </div>
+    )
+}
+
+export default ComingSoon
